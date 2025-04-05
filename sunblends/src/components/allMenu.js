5aa2694b72aad8851711582f68cadee303fa07a7
devshiru <125737044+mastershiru@users.noreply.github.com>
2025-04-05 15:40:49 +0800

    Update allMenu.js
    fix all menu

--- sunblends/src/components/allMenu.js
+++ sunblends/src/components/allMenu.js
@@ -46,7 +46,7 @@ const AllMenu = () => {
   } = useNavbar();
 
   // API URL
-  const API_BASE_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000/api";
+  const API_BASE_URL = "https://api.sunblends.store/api";
 
   
 
